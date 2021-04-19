108b4a9af42d971b886dba7dfa5f8d2c80e3b31b
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2021-04-19 18:59:47 -0300

    controlling prompt install screen

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,14 @@
+var deferredPrompt;
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js").then(() => {
     console.log("service worker registered");
   });
 }
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  console.log("beforeinstallprompt fired");
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
