f994b6fb4dbe107726cad72b5407357dca2d4fff
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2021-04-22 19:42:04 -0300

    caching

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,9 +1,16 @@
 var deferredPrompt;
 
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js").then(() => {
-    console.log("service worker registered");
-  });
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(() => {
+      console.log("service worker registered");
+    })
+    .catch((error) => console.log(error));
 }
 
 window.addEventListener("beforeinstallprompt", (event) => {
