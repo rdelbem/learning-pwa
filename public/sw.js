8f694d09ef4bb9ecc10abb51653b0d5ce85a69cf
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2021-05-06 00:28:26 -0300

    added all shell caching

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,21 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("static").then((cache) => {
       console.log("[service worker] precaching app shell");
-      cache.add("/src/js/app.js");
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/promise.js",
+        "/src/js/fetch.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+      ]);
     })
   );
 });
