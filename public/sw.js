07081abba40b40d7cf64bc334bb18927c2dd992e
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2021-05-08 00:28:42 -0300

    dynamic caching

--- public/sw.js
+++ public/sw.js
@@ -33,7 +33,12 @@ self.addEventListener("fetch", (event) => {
       if (response) {
         return response;
       } else {
-        return fetch(event.request);
+        return fetch(event.request).then((res) => {
+          return caches.open("dynamic").then((cache) => {
+            cache.put(event.request.url, res.clone());
+            return res;
+          });
+        });
       }
     })
   );
