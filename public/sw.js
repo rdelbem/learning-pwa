13738f8cfa825dc56e7d6df2069b9932036c2f28
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2021-05-12 23:49:51 -0300

    error handling on console

--- public/sw.js
+++ public/sw.js
@@ -33,12 +33,14 @@ self.addEventListener("fetch", (event) => {
       if (response) {
         return response;
       } else {
-        return fetch(event.request).then((res) => {
-          return caches.open("dynamic").then((cache) => {
-            cache.put(event.request.url, res.clone());
-            return res;
-          });
-        });
+        return fetch(event.request)
+          .then((res) => {
+            return caches.open("dynamic").then((cache) => {
+              cache.put(event.request.url, res.clone());
+              return res;
+            });
+          })
+          .catch((error) => {});
       }
     })
   );
